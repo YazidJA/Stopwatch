bafb7a4e08cc2f996f18dba4f9258dddc036ee17
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-13 02:57:47 +0100

    Time format
    Time formatted as [m : s : ms]

--- index.js
+++ index.js
@@ -6,18 +6,29 @@ const timer = document.getElementById("timer");
 
 let started,
   stopwatch,
-  duration = 0;
+  duration = [0, 0, 0]; // miliseconds, seconds, minutes
 
 showtimer = function () {
-  duration++;
-  timer.textContent = parseFloat(duration / 10);
+  if (duration[1] < 60) {
+    if (duration[0] < 60) {
+      duration[0]++; //increments ms
+    } else {
+      duration[0] = 0; //resets ms
+      duration[1]++; // increments secs
+    }
+  } else {
+    duration[1] = 0; // resets secs
+    duration[2]++; // increments min
+  }
+
+  timer.textContent = `${duration[2]} : ${duration[1]} : ${duration[0]}`; // output
 };
 
 start = function () {
   if (!started) {
     log.textContent = "STOPWATCH IS NOW RUNNING";
     started = true;
-    stopwatch = setInterval(showtimer, 100);
+    stopwatch = setInterval(showtimer, 10);
   } else {
     log.textContent = "STOPWATCH IS ALREADY RUNNING !!";
   }
@@ -34,10 +45,10 @@ stop = function () {
 };
 
 reset = function () {
-  log.textContent = "";
+  log.textContent = "STOPWATCH STATUS";
   clearInterval(stopwatch);
-  duration = 0;
-  timer.textContent = "";
+  duration = [0, 0, 0];
+  timer.textContent = `00 : 00 : 00`;
 };
 
 startBtn.addEventListener("click", start);
